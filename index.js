103f2f08eeefc68175e45ec30d4908c52684c7b3
BigJonny <32932457+CAHO15@users.noreply.github.com>
2024-10-30 14:59:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 Draw.loadPlugin(function(ui) {
     ui.actions.addAction('myOrganicFlow', function() {
         var e = ui.editor.graph;
+        var layout = new mxFastOrganicLayout(e);
+        layout.execute(e.getDefaultParent());
     }, null, null, "Ctrl+Shift+Q");
 
     ui.keyHandler.bindAction(81, !0, "myOrganicFlow", !0);
