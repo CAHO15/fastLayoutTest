c7c30a48f5625e2d24f9b8179287ba02f847771f
BigJonny <32932457+CAHO15@users.noreply.github.com>
2024-10-30 15:04:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 Draw.loadPlugin(function(ui) {
+    mxResources.parse('myOrganicFlow=OrganicFlow');
     ui.actions.addAction('myOrganicFlow', function() {
         var e = ui.editor.graph;
         var layout = new mxFastOrganicLayout(e);
@@ -7,4 +8,9 @@ Draw.loadPlugin(function(ui) {
     }, null, null, "Ctrl+Shift+Q");
 
     ui.keyHandler.bindAction(81, !0, "myOrganicFlow", !0);
+    ui.menubar.addMenu('My Menu', function(menu, parent) {
+        ui.menus.addMenuItem(menu, 'myOrganicFlow');
+    });
+    ui.menubar.container.insertBefore(ui.menubar.container.lastChild,
+    ui.menubar.container.lastChild.previousSibling.previousSibling);
 });
